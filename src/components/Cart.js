bc3672882a8dc210815654cca2cd1526583119f2
Sergio Camacho <ser.a.camacho93@protonmail.com>
2021-07-12 21:42:42 -0500

    fixed non-used var Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons"
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import { motion } from "framer-motion"
